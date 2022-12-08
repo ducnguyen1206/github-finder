3f28f319d6da871a22bdb0d08a8204c81e0e8d6a
dnguyen292 <ducminh.nguyen@dxc.com>
2022-12-08 12:25:53 +0700

    first commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
+
+const GithubContex = createContext();
+
+export const GithubProvider = ({ children }) => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false,
+    };
+
+    const [state, dispatch] = useReducer(githubReducer, initialState);
+
+    return (
+        <GithubContex.Provider
+            value={{
+                ...state,
+                dispatch,
+            }}
+        >
+            {children}
+        </GithubContex.Provider>
+    );
+};
+
+export default GithubContex;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from '../components/User/UserResults';
+import UserSearch from '../components/User/UserSearch';
+
+function Home() {
+    return (
+        <>
+            <UserSearch />
+            <UserResults />
+        </>
+    );
+}
+
+export default Home;
